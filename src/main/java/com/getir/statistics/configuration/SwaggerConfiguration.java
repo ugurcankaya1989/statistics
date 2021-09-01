ab29bf966e80e96d84dc994d78272f1d7927a7fe
ugurcankaya <ugur.cankaya1989@gmail.com>
2021-09-01 11:25:40 +0300

    Swagger Configuration for authorization

--- src/main/java/com/getir/statistics/configuration/SwaggerConfiguration.java
+++ src/main/java/com/getir/statistics/configuration/SwaggerConfiguration.java
@@ -1,5 +1,6 @@
 package com.getir.statistics.configuration;
 
+import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -7,16 +8,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 @EnableSwagger2
 @PropertySource("classpath:swagger.properties")
 @Configuration
 public class SwaggerConfiguration implements WebMvcConfigurer {
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String DEFAULT_INCLUDE_PATTERN = "/api/.*";
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder().title("Statistics API")
@@ -29,7 +34,29 @@ public class SwaggerConfiguration implements WebMvcConfigurer {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().apis(RequestHandlerSelectors.basePackage("com.getir.statistics.controller")).paths(PathSelectors.any()).build();
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
+                .securityContexts(Lists.newArrayList(securityContext()))
+                .securitySchemes(Lists.newArrayList(apiKey())).select()
+                .apis(RequestHandlerSelectors.basePackage("com.getir.statistics.controller")).paths(PathSelectors.any()).build();
     }
 
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex(DEFAULT_INCLUDE_PATTERN))
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(
+                new SecurityReference("JWT", authorizationScopes));
+    }
 }
